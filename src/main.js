d5e1738ac0d68e65462fa0e2abff970c2f93fb6e
Tibe Calderon <tibecvp@gmail.com>
2022-10-12 20:38:45 -0500

    Delete commented old function that did not work

--- src/main.js
+++ src/main.js
@@ -1,18 +1,3 @@
-// async function getTrendingMoviesPreview() {
-//     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day', {
-//     method: "GET",
-//     mode: 'no-cors',
-//     headers: {
-//         'Content-Type': 'application/json',
-//         "x-api-key": API_KEY
-//     }
-//     });
-//     const data = await res.json();
-
-//     const movies = data.results;
-//     console.log({data, movies});
-// }
-
 async function getTrendingMoviesPreview() {
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
     const data = await res.json();
